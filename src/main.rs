87aa949d46aaf4c7aab1e9b5ab3b405937bb9540
ExplorerSec <gaoweixu@foxmail.com>
2025-04-16 23:29:33 +0800

    仅进行了文件的拆分

--- src/main.rs
+++ src/main.rs
@@ -1,79 +1,12 @@
-use std::{env, collections::HashMap};
+use std::env;
+mod url_basic;
 
-fn url_encode(s:&str)->String{
-    let map: HashMap<&str, &str> = HashMap::from([
-        (" ", "%20"),
-        ("!", "%21"),
-        ("\"", "%22"),
-        ("'", "%27"),
-        ("(", "%28"),
-        (")", "%29"),
-        ("*", "%2A"),
-        ("+", "%2B"),
-        (",", "%2C"),
-        ("/", "%2F"),
-        (":", "%3A"),
-        (";", "%3B"),
-        ("<", "%3C"),
-        (">", "%3E"),
-        ("@", "%40"),
-        ("[", "%5B"),
-        ("]", "%5D"),
-        ("\\", "%5C"),
-        ("_", "%5F"),
-        ("^", "%5E"),
-        ("`", "%60"),
-        ("{", "%7B"),
-        ("|", "%7C"),
-        ("}", "%7D"),
-        //("~", "%7E"),
-        ]);
-        let mut out = String::from(s);
-        for i in map{
-            out = out.replace(i.0, i.1);
-        }
-        out
+use url_basic::url_encode;
+use url_basic::url_decode;
 
-}
-
-fn url_decode(s:&str)->String{
-    let map: HashMap<&str, &str> = HashMap::from([
-        ("%20"," "),
-        ("%21","!"),
-        ("%22","\""),
-        ("%27","'"),
-        ("%28","("),
-        ("%29",")"),
-        ("%2A","*"),
-        ("%2B","+"),
-        ("%2C",","),
-        ("%2F","/"),
-        ("%3A",":"),
-        ("%3B",";"),
-        ("%3C","<"),
-        ("%3E",">"),
-        ("%40","@"),
-        ("%5B","["),
-        ("%5D","]"),
-        ("%5C","\\"),
-        ("%5F","_"),
-        ("%5E","^"),
-        ("%60","`"),
-        ("%7B","{"),
-        ("%7C","|"),
-        ("%7D","}"),
-        ("%7E","~"),
-        ]);
-        let mut out = String::from(s);
-        for i in map{
-            out = out.replace(i.0, i.1);
-        }
-        out
-    }
 
 fn show_help(){
-    println!("\u{001b}[1;42mUrlCode 0.0.1, By ExpZero\u{001b}[0m"); // 使用颜色转义
-    println!("");
+    print!("\u{001b}[1;42mUrlCode 0.0.1, By ExpZero\u{001b}[0m\n\n"); // 使用颜色转义
     println!("Usage: UrlCode [Command] [Url String]");
     println!("Command:");
     println!("    e|-e|encode|-encode|--encode        encode the url");
@@ -81,7 +14,6 @@ fn show_help(){
     println!("Url String:");
     println!("    the string to encode or decode.");
 }
-
 fn main() {
     let args:Vec<String> = env::args().collect();
     match args.len(){
